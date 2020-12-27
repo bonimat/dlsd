8b0a97b82d7ccb5741ea7fb4a82bd7e523fdd551
Matteo Boni <bonimat@hotmail.com>
2020-12-27 10:22:29 +0100

    Check simple angnostic query

--- test/database.js
+++ test/database.js
@@ -1,5 +1,18 @@
-describe('Database: test it Works', function () {
-    it('it works', function (done) {
-        done();
+const { expect } = require('chai');
+const { pool } = require('./../dbConfig');
+
+describe('Database Tests', function () {
+    describe('Test works', function () {
+        it('it works', function (done) {
+            done();
+        });
+    });
+
+    describe('Test connection', function () {
+        it('Auth connection', async function () {
+            const result = await pool.query('select 1 as result');
+            console.log(result.rows[0].result);
+            expect(result.rows[0].result).to.be.equal(1);
+        });
     });
 });
